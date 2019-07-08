f259fc1e487ebd5ae37f4abac5397cf6954f0e39
Victória L. Siqueira Cavalcanti <50150571+vitsiqueira@users.noreply.github.com>
2019-07-08 15:52:41 -0300

    Add files via upload

--- F1.py
+++ F1.py
@@ -7,24 +7,24 @@ im = [] #CONJUNTO DAS IMAGENS
 numerador = 1 #NUMERADOR DE PARES
 N = int(input("Quantidade de pares que serão adicionados: ")) #INPUT PARA A QUANTIDADE DE PARES
 for i in range(N): #REPETIÇÃO
-    D = ("""Valor do {}º domínio:  
+    D = input("""Valor do {}º domínio:  
     Se não houver, digite "nulo"
     ::: """.format(numerador)) # INPUT PARA VALORES DO DOMÍNIO COM O CONTADOR DE PARES
-    I = ("""Valor da {}ª imagem:
+    I = input("""Valor da {}ª imagem:
     Se não houver, digite "nulo"
     ::: """.format(numerador)) #INPUT PARA VALORES DA IMAGEM COM O CONTADOR DE PARES
-    dom.append(D) #ADICIONANDO VALORES DO DOMÍNIO À LISTA DOM 
+    dom.append(D) #ADICIONANDO VALORES DO DOMÍNIO À LISTA DOM
     im.append(I) #ADICIONANDO VALORES DA IMGAEM À LISTA IM
-    cont += 1 #CONTADOR SOBE UM
     numerador += 1 #NUMERADOR SOBE UM
+    cont += 1 #CONTADOR SOBE UM
 
-    if len(im) == len(set(im)): #FUNÇÃO LEN PARA CONTAR ELEMENTOS DA LISTA | SET PARA CHECAR SE HÁ VALORES REPETIDOS
-        injetora += 1 
-    if im == "nulo": 
+    if len(set(im)) == len(im): #FUNÇÃO LEN PARA CONTAR ELEMENTOS DA LISTA | SET PARA CHECAR SE HÁ VALORES REPETIDOS
+        injetora += 1
+    if im == "nulo":
         sobrejetora += 1
 
 if sobrejetora != 0 and injetora != 0: #CONDIÇÃO PARA UMA FUNÇÃO INJETORA
-    print("É uma função injetora.") 
+    print("É uma função injetora.")
 if sobrejetora == 0 and injetora == 0: #CONDIÇÃO PARA UMA FUNÇÃO SOBREJETORA
     print("É uma função sobrejetora.")
 if sobrejetora == 0 and injetora != 0: #CONDIÇÃO PARA UMA FUNÇÃO BIJETORA
