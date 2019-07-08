623403c77a559cc5a14fd08422285d69be0c3d79
Victória L. Siqueira Cavalcanti <50150571+vitsiqueira@users.noreply.github.com>
2019-07-08 15:56:26 -0300

    Update L1.py

--- L1.py
+++ L1.py
@@ -1,4 +1,5 @@
-def lógica():
+# L1 :::::::::::::::::::::::::::::::::::::
+def lógica(): #DEF PARA O MENU DO PROGRAMA
 
     print("="*80)
     print("")
@@ -21,7 +22,7 @@ Escolha uma operação lógica:"""))
     print("")
     print("="*80)
 
-    if res == 1:
+    if res == 1: #CONDIÇÃO PARA CONJUNÇÕES
         if p == 1 and q == 1:
             print("Verdadeiro!")
         if p == 1 and q == 0:
@@ -31,7 +32,7 @@ Escolha uma operação lógica:"""))
         if p == 0 and q == 0:
             print("Falso!")
 
-    if res == 2:
+    if res == 2: #CONDIÇÃO PARA DISJUNÇÕES
         if p == 1 and q == 1:
             print("Verdadeiro!")
         if p == 1 and q == 0:
@@ -41,7 +42,7 @@ Escolha uma operação lógica:"""))
         if p == 0 and q == 0:
             print("Falso!")
 
-    if res == 3:
+    if res == 3: #CONDIÇÕES PARA OU EXCLUSIVO
         if p == 1 and q == 1:
             print("Falso!")
         if p == 1 and q == 0:
@@ -51,7 +52,7 @@ Escolha uma operação lógica:"""))
         if p == 0 and q == 0:
             print("Falso!")
 
-    if res == 4:
+    if res == 4: #CONDIÇÕES PARA CONDICIONAIS (HAHA)
         if p == 1 and q == 1:
             print("Verdadeiro!")
         if p == 1 and q == 0:
@@ -61,7 +62,7 @@ Escolha uma operação lógica:"""))
         if p == 0 and q == 0:
             print("Verdadeiro!")
 
-    if res == 5:
+    if res == 5: #CONDIÇÕES PARA BICONDICIONAIS
         if p == 1 and q == 1:
             print("Verdadeiro!")
         if p == 1 and q == 0:
@@ -72,4 +73,4 @@ Escolha uma operação lógica:"""))
             print("Verdadeiro!")
     pass
 
-lógica()
+lógica() #EXECUTAR A DEF
